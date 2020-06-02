4bc8120bb9e894d06664858963d790bcda66fea4
DanPaddock <danpaddk@gmail.com>
2020-06-02 12:47:17 -0400

    weapon zoom and decreasing ammo

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomOutFOV = 60f;
+    [SerializeField] float zoomInFOV = 25f;
+    [SerializeField] float zoomOutSens = 2f;
+    [SerializeField] float zoomInSens = 0.5f;
+
+    RigidbodyFirstPersonController fpsController;
+
+    bool isZoomed = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(isZoomed == false)
+            {
+                isZoomed = true;
+                fpsCamera.fieldOfView = zoomInFOV;
+                fpsController.mouseLook.XSensitivity = zoomInSens;
+                fpsController.mouseLook.YSensitivity = zoomInSens;
+            }
+            else
+            {
+                isZoomed = false;
+                fpsCamera.fieldOfView = zoomOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomOutSens;
+                fpsController.mouseLook.YSensitivity = zoomOutSens;
+            }
+        }
+    }
+
+}
